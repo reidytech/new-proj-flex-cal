07d9323a8b7cbfcafeeb419a5deec7c87d7a1ccd
U-SVRN\x876007 <reidy.data@gmail.com>
2017-09-13 12:06:40 -0400

    colors and values

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+/* constant sample data */
+
 var DATA = {
-    projectNames: ["Project-1", "Project-2", "Project-3", "Project-4", "Project-5", "Project-6", "Project-7", "Project-8", "Project-9"]
+    projectNames: ["Project-1", "Project-2", "Project-3", "Project-4", "Project-5", "Project-6", "Project-7", "Project-8", "Project-9", "Project-11", "Project-12", "Project-13", "Project-14", "Project-15"]
 }
 
-function genRandomData() {
-    var arr = [];
-    for(var i = 0; i < 60; i++){
-        arr.push(i);
+var fteARR = [];
+
+var accessDataCellArr = {
+    dataCellArr: this.dataCellArr,
+    addCell: function(cell) {
+        dataCellArr.push(cell);
+    },
+    /*removeCell: function(cell) {
+        dataCellArr.pop(cell);
+    }*/
+    returnCellArr: function() {
+        return this.dataCellArr;
     }
-    return arr;
 }
+
+accessDataCellArr.dataCellArr = [];
+console.log(accessDataCellArr.dataCellArr);
+
+function genRandomData(arr) {
+    fteARR.push(arr);
+}
+
+function getRandomData() {
+    var stoArr = [];
+    for(var j = 0; j < 60; j++){
+        var sampleData = Math.floor((Math.random() * 16) + 1);
+        stoArr.push(sampleData);
+    }
+    return stoArr;
+}
+
 
 function populateWeekNumbers(){
     var weekNumberTable = document.querySelector('.week-number-table');
@@ -19,39 +45,69 @@ function populateWeekNumbers(){
     for(var i = 0; i < 60; i++){
         var weekNumberCell = document.createElement('td');
         weekNumberCell.style.fontWeight = 'bold';
-        weekNumberCell = elemAttr(weekNumberCell, {id: `week-id-${i+1}`})
+        weekNumberCell = elemAttr(weekNumberCell, {id: `week-id-${i+1}`});
+        weekNumberCell.className = 'week-cal-cells';
         weekNumberCell.innerHTML = (i+1).toString();
-        weekNumberCell.style.borderTop = '2px solid black';
-        weekNumberCell.style.borderBottom = '2px solid black';
-        weekNumberCell.style.borderRight = '2px solid black';
-        weekNumberCell.style.minWidth = '1.5em';
-        weekNumberCell.style.textAlign = 'center';
         weekNumberRow.appendChild(weekNumberCell);
     } 
 }
+
+/* this function populates project names (leftmost, fixed column) */
 
 function populateProjectNames(DATA){
     var projectNameTable = document.querySelector('.project-names');
     var projectNames = DATA.projectNames;
     var projNodeMap = projectNames.map((val) => {
-        console.log(val);
         var nameCell = document.createElement('td');
         nameCell.innerHTML = val;
-        nameCell.style.width = '100%';
-        nameCell.style.borderTop = '2px solid black';
-        nameCell.style.borderBottom = '2px solid black';
-        nameCell.style.fontWeight = 'bold';
+        nameCell.className = 'proj-name-cell';
+        //this currently sets the id of the cell to the project number, should be changed to project
+        //name for effective data setting-getting
         nameCell = elemAttr(nameCell, {id: `proj-${val.match(/[0-9]/g)}`})
         return nameCell;
     });
-
-    console.log(projNodeMap);
-
+    
     for(projNode of projNodeMap){
         tableRow = document.createElement('tr');
         tableRow.appendChild(projNode);
         //projectNameTable.appendChild(document.createElement('tr').appendChild(projNode));
         projectNameTable.appendChild(tableRow);
+    }
+}
+
+function populateFTE(fteARR){
+    for(var i = 0; i < DATA.projectNames.length; i++){
+        var weekDataProjRow = document.createElement('tr');
+        weekDataProjRow = elemAttr(weekDataProjRow, {class: DATA.projectNames[i].toLowerCase()});
+        for(var j = 0; j < fteARR[i].length; j++){
+            var weekDataCell = document.createElement('td');
+            weekDataCell = elemAttr(weekDataCell, {id: `${DATA.projectNames[i].toLowerCase()} week-num-${j + 1}`});
+            weekDataCell.className = 'fte-cell';
+            weekDataProjRow.appendChild(weekDataCell);
+            weekDataCell.innerHTML = (fteARR[i][j]).toString();
+            document.querySelector('.week-number-table').appendChild(weekDataProjRow);
+            accessDataCellArr.dataCellArr.push(weekDataCell);
+        }
+    }
+}
+
+function fteAlerts(){
+    var index = 0;
+    while(index < accessDataCellArr.dataCellArr.length) {
+        if(accessDataCellArr.dataCellArr[index].innerHTML >= 10){
+            accessDataCellArr.dataCellArr[index].style.background = 'red';
+        }
+        else if(accessDataCellArr.dataCellArr[index].innerHTML >= 7 && accessDataCellArr.dataCellArr[index].innerHTML < 10){
+            accessDataCellArr.dataCellArr[index].style.background = 'orange';
+        }
+        else if(accessDataCellArr.dataCellArr[index].innerHTML >= 3 && accessDataCellArr.dataCellArr[index].innerHTML < 7){
+            accessDataCellArr.dataCellArr[index].style.background = 'yellow';
+        }
+        else {
+            accessDataCellArr.dataCellArr[index].style.background = 'green';
+        }
+
+        index++;
     }
 }
 
@@ -71,9 +127,25 @@ function elemAttr(node, classObj){
 
 populateWeekNumbers();
 populateProjectNames(DATA);
+for(var i = 0; i < 16; i++){
+    genRandomData(getRandomData());
+}
+
+populateFTE(fteARR);
+fteAlerts();
 
 
 
+
+
+console.log(fteARR);
+
+
+
+
+
+/* <tr><td style="">1</td></tr> */
+/* tr - class needs to be project name | td - cell needs to have project name as ID and week number as ID td needs class as fteCell*/
 
 /*
 
